a8d1067de100e48e15a24406d0865bc3ed894607
Connor <cjshore12@gmail.com>
2021-02-06 14:48:31 -0700

    Added premake build script

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Candle"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Candle"
+	location "Candle"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CANDLE_PLATFORM_WINDOWS",
+			"CANDLE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir.. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "CANDLE_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "CANDLE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CANDLE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Candle/vendor/spdlog/include",
+		"Candle/src"
+	}
+
+	links
+	{
+		"Candle"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CANDLE_PLATFORM_WINDOWS"
+		}
+	
+	filter "configurations:Debug"
+		defines "CANDLE_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "CANDLE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CANDLE_DIST"
+		optimize "On"
